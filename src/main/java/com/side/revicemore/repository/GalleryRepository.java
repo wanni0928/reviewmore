8d871975794c7c819994b6e624d60bc1ca4448fc
wanni0928 <chlgudwnd123@gmail.com>
2020-10-26 22:44:48 +0900

    Update GalleryRepository.java

--- src/main/java/com/side/revicemore/repository/GalleryRepository.java
+++ src/main/java/com/side/revicemore/repository/GalleryRepository.java
@@ -1,9 +1,11 @@
 package com.side.revicemore.repository;
 
 import com.side.revicemore.domain.Gallery;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface GalleryRepository {
     List<Gallery> findAllGalleries();
     void insertGallery(Long id, String title, String content);

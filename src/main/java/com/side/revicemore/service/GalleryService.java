738e5403d090d1f90f3028dd09c6364b28c36ffe
wanni0928 <chlgudwnd123@gmail.com>
2020-10-27 00:21:15 +0900

    Update GalleryService.java

--- src/main/java/com/side/revicemore/service/GalleryService.java
+++ src/main/java/com/side/revicemore/service/GalleryService.java
@@ -1,37 +1,64 @@
 package com.side.revicemore.service;
 
 import com.side.revicemore.domain.Gallery;
-import com.side.revicemore.repository.GalleryMapper;
+import com.side.revicemore.domain.Member;
+import com.side.revicemore.repository.GalleryRepository;
+import com.side.revicemore.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class GalleryService {
-    private final GalleryMapper galleryMapper;
+    private final GalleryRepository galleryRepository;
+    private final MemberRepository memberRepository;
 
-    public GalleryService(GalleryMapper galleryMapper) {
-        this.galleryMapper = galleryMapper;
+    /**
+     * 게시글 작성
+     */
+    @Transactional
+    public Long write(Long memberId, String title, String content) {
+        // 엔티티 조회
+        Member member = memberRepository.findById(memberId).orElse(null);
+
+        // 게시판 내용 생성
+        Gallery gallery = new Gallery();
+        gallery.setAuthor(member.getMemberAccount());
+        gallery.setContent(content);
+        gallery.setTitle(title);
+
+        //게시판 저장
+        galleryRepository.save(gallery);
+        return gallery.getGalleryId();
     }
 
-
-    public List<Gallery> findAllGalleries() {
-        return galleryMapper.findAllGalleries();
+    /**
+     * 게시판 삭제
+     */
+    @Transactional
+    public void deleteGallery(Long galleryId){
+        Gallery gallery = galleryRepository.findById(galleryId).orElse(null);
+        // 게시판 삭제
     }
 
-    public void insertGallery(Long memberId, String title, String content) {
-        galleryMapper.insertGallery(memberId, title, content);
+    /**
+     * 게시판 수정
+     */
+    @Transactional
+    public void updateGallery(Long galleryId, String title, String content) {
+        Gallery gallery = galleryRepository.findById(galleryId).orElse(null);
+        gallery.setTitle(title);
+        gallery.setContent(content);
     }
 
-    public Gallery findById(Long galleryId) {
-        return galleryMapper.findById(galleryId);
-    }
-
-    public void deleteById(Long id){
-        galleryMapper.deleteById(id);
-    }
-
-    public void updateById(Long id, String title, String content) {
-        galleryMapper.updateById(id, title, content);
+    /**
+     * 게시판 목록
+     */
+    public List<Gallery> findGalleries() {
+        return galleryRepository.findAll();
     }
 }

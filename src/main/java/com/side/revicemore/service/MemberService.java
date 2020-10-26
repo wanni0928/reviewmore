f436b7a069c6306d9403eee2de9793d92964d591
wanni0928 <chlgudwnd123@gmail.com>
2020-10-27 00:17:21 +0900

    Update MemberService.java

--- src/main/java/com/side/revicemore/service/MemberService.java
+++ src/main/java/com/side/revicemore/service/MemberService.java
@@ -2,32 +2,51 @@ package com.side.revicemore.service;
 
 import com.side.revicemore.domain.Member;
 import com.side.revicemore.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    /**
+     * 회원가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        // 같은 이름이 있는지 확인
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getMemberId();
     }
 
-    public List<Member> findMembers(){
-        return memberRepository.selectMembers();
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getMemberAccount())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 
-    public Member findOne(Long memberId) {
-        return memberRepository.findMemberById(memberId);
+    public void updateMember(Long memberId, String memberAccount, String memberPassword) {
+        Member member = findOne(memberId);
+        member.setMemberAccount(memberAccount);
+        member.setMemberPassword(memberPassword);
     }
 
-    public void insert(String account, String password){
-        memberRepository.insert(account, password);
+    /**
+     *  전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
     }
 
-    public HashMap<String, Object> findGalleryByMemberId(Long memberId){
-        return memberRepository.findGalleryByMemberId(memberId);
+    public Member findOne(Long id) {
+        return memberRepository.findById(id).orElse(null);
     }
+
 }

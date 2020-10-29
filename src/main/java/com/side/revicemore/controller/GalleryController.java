ea132bc274018496e582eba5ed2d3c22baa6863b
wanni0928 <chlgudwnd123@gmail.com>
2020-10-29 20:39:25 +0900

    Update GalleryController.java

--- src/main/java/com/side/revicemore/controller/GalleryController.java
+++ src/main/java/com/side/revicemore/controller/GalleryController.java
@@ -1,11 +1,12 @@
 package com.side.revicemore.controller;
 
-import com.side.revicemore.domain.Gallery;
+import com.side.revicemore.controller.form.GalleryForm;
 import com.side.revicemore.service.GalleryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,12 +15,20 @@ public class GalleryController {
 
     @GetMapping(value = "/galleries/new")
     public String createForm(Model model) {
-        model.addAttribute("form", new Gallery());
+        model.addAttribute("form", new GalleryForm());
         return "gallery/createForm";
     }
 
-//    @PostMapping(value = "/galleries/new")
-//    public String create() {
-//
-//    }
+    @PostMapping(value = "/galleries/new")
+    public String create(GalleryForm form) {
+        Long memberId = 56L;
+        galleryService.write(memberId, form.getTitle(), form.getContent());
+        return "redirect:/galleries";
+    }
+
+    @GetMapping(value = "/galleries")
+    public String list(Model model) {
+        model.addAttribute("galleries", galleryService.findGalleries());
+        return "gallery/galleryList";
+    }
 }

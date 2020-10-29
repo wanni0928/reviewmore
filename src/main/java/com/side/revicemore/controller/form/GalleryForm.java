1bdc5358908300ee0724ac1db7103d54a7e9aec4
wanni0928 <chlgudwnd123@gmail.com>
2020-10-29 20:39:22 +0900

    Create GalleryForm.java

---
+++ src/main/java/com/side/revicemore/controller/form/GalleryForm.java
@@ -0,0 +1,10 @@
+package com.side.revicemore.controller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class GalleryForm {
+    private String title;
+    private String content;
+}
